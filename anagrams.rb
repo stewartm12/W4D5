1576e749f53fa7d225a54cb4d559c8957e834ac2
Lijun <gan.lijun.glj@gmail.com>
2021-01-22 15:32:27 -0500

    create # anagram

--- anagrams.rb
+++ anagrams.rb
@@ -7,13 +7,37 @@ end
 # p first_anagram?("elvis", "lives")
 
 
-def second_anagram2?(string_1, string_2)
+def second_anagram?(string_1, string_2)
   string_1.each_char do |char| 
-    string_2.delete!(string_2.index(char))
+    if !string_2.include?(char)
+      return false 
+    else
+      idx = string_2.index(char)
+      string_2.delete!(string_2[idx])
+    end 
   end
-
   string_2.chars.empty?
 end
 
-p second_anagram2?("gizmo", "sally") 
-p first_anagram2?("elvis", "lives")
+# p second_anagram?("gizmo", "sally") 
+# p second_anagram?("elvis", "lives")
+
+def third_anagram?(string_1, string_2)
+  first = string_1.chars.sort.join("")
+  second = string_2.chars.sort.join("")
+  first == second
+end 
+
+# p third_anagram?("gizmo", "sally") 
+# p third_anagram?("elvis", "lives")
+
+
+def fourth_anagram?(string_1,string_2)
+  hash = Hash.new(0)
+  string_1.each_char {|char| hash[char] += 1 }
+  string_2.each_char {|char| hash[char] -= 1 }
+  hash.all?{|k,v| v == 0}
+end 
+
+p fourth_anagram?("gizmo", "sally") 
+p fourth_anagram?("elvis", "lives")
