84356829aad7818110c0fb9bc6b610a96d131341
Lijun <gan.lijun.glj@gmail.com>
2021-01-22 14:31:47 -0500

    Creat #largest_contiguous_subsum2

--- execution_time.rb
+++ execution_time.rb
@@ -1,19 +1,30 @@
 #Phase 1 => O(n^2)
 
+# def my_min(arr)
+#   smallest = arr[0]
+#   (0...arr.length).each do |i|
+#     (0...arr.length).each do |j|
+#       if arr[i] < arr[j] && arr[i] < smallest
+#         smallest = arr[i] 
+#       end
+#     end
+#   end 
+#   smallest 
+# end
+
 def my_min(arr)
   smallest = arr[0]
-  (0...arr.length).each do |i|
-    (0...arr.length).each do |j|
-      if arr[i] < arr[j] && arr[i] < smallest
-        smallest = arr[i] 
+  arr.each do |i|
+    arr.each do |j|
+      if i < j 
+        smallest = i
       end
     end
   end 
-
   smallest 
-end
+end 
 
-# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
 
 #Phase 2 => O(n)
@@ -24,10 +35,42 @@ def my_min2(arr)
   smallest 
 end
 
-p my_min2([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+# p my_min2([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 
 #
 
 def largest_contiguous_subsum(arr)
-  
+  subs = []
+  arr.each.with_index do |ele1,i1|
+    arr.each.with_index do |ele2,i2|
+      subs << arr[i1..i2] if i2 >= i1
+    end 
+  end  
+  subs.map{|ele| ele.sum}.max
 end
+
+list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+
+def largest_contiguous_subsum2(arr)
+  large_sum = arr.first
+  current_sum = arr.first
+  i = 0
+  j = 0
+  while i < arr.length
+    current_sum = arr[i..j].sum
+    if current_sum > large_sum
+      large_sum = current_sum
+    end 
+    if j == arr.length - 1
+      i += 1
+      j = 0
+    end 
+    j += 1
+  end 
+  large_sum
+end 
+
+
+# p largest_contiguous_subsum2(list) # => 8 (from [7, -6, 7])
